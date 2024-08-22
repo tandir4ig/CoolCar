4ddad37804cd4de08b2fdf64a8844339c89b90f8
arslan.saidov.2002 <arslan.saidov.2002@gmail.com>
2024-08-22 15:46:50 +0300

    11

---
+++ CoolCar/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using CoolCar.Controllers;
+using CoolCar.Models;
+using CoolCar.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace CoolCar.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class UserController : Controller
+    {
+        private readonly IUserInterface userInterface;
+        private readonly IRoleInterface roleInterface;
+        public UserController(IUserInterface userInterface, IRoleInterface roleInterface)
+        {
+            this.userInterface = userInterface;
+            this.roleInterface = roleInterface;
+        }
+        public IActionResult Index()
+        {
+            var Users = userInterface.GetAll();
+            return View(Users);
+        }
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(Register reg)
+        {
+            if (userInterface.TryGetByName(reg.UserName) != null)
+            {
+                ModelState.AddModelError("", "Пользователь с таким именем уже существует");
+                return View();
+            }
+
+            if (reg.Password == reg.UserName)
+            {
+                ModelState.AddModelError("", "Пароль и логин не должны совпадать");
+                return View();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            userInterface.Add(new User(reg.UserName, reg.Password, reg.FirstName, reg.LastName, reg.Phone));
+            return RedirectToAction(nameof(HomeController.Catalog), "Home");
+        }
+    }
+}
