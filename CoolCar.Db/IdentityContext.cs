3e8c8015ada2d3a13a8a112da50f4821614c792c
Arslan.Saidov <arslan.saidov.2002@gmail.com>
2024-09-08 14:43:33 +0300

    123

---
+++ CoolCar.Db/IdentityContext.cs
@@ -0,0 +1,15 @@
+﻿using CoolCar.Db.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoolCar.Db
+{
+    public class IdentityContext : IdentityDbContext<User>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) 
+            : base(options)
+        {
+            Database.Migrate(); 
+        }
+    }
+}
